1c14f0c9d43ca7938c303e794e55f6e6fa8f4544
itsmina1 <mina.adham1999@gmail.com>
2021-06-02 23:17:52 -0700

    sh file

---
+++ os8.sh
@@ -0,0 +1,14 @@
+mkdir testdoc!
+chmod 397 testdoc!
+chmod 440 testdoc!
+mkdir file1
+mkdir sample
+chmod g=w sample
+chmod r+x sample
+chmod: invalid mode: ‘r+x’
+mkdir test
+chmod u+g test
+/////////////////////////////////
+chmod -c
+chmod --version
+chmod -R
